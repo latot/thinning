059fd65d295bc7e601ab6a50ad6453a18f58d6cd
Laurențiu Nicola <lnicola@dend.ro>
2023-04-15 18:56:09 +0300

    Fix tiled mode for GRASS

--- src/main.rs
+++ src/main.rs
@@ -47,6 +47,7 @@ fn thinning_zs_iteration(
     win_h: usize,
     w: usize,
     h: usize,
+    r: usize,
 ) -> bool {
     let mut diff: bool = false;
     let min_x = if win_x == 0 { 1 } else { win_x };
@@ -61,42 +62,38 @@ fn thinning_zs_iteration(
     } else {
         win_y + win_h
     };
-    for r in 1..5 {
-        let ind1: usize;
-        let ind2: usize;
-        let ind3: usize;
+    let ind1: usize;
+    let ind2: usize;
+    let ind3: usize;
 
-        ind1 = r - 1;
+    ind1 = r - 1;
 
-        if r <= 3 {
-            ind2 = r;
-        } else {
-            ind2 = 0;
-        }
+    if r <= 3 {
+        ind2 = r;
+    } else {
+        ind2 = 0;
+    }
 
-        ind3 = (r - 1) + 4;
+    ind3 = (r - 1) + 4;
 
-        for i in min_y..max_y {
-            for j in min_x..max_x {
-                let p1: u8 = im[i * w + j] & 1;
-                if p1 == 0 {
-                    continue;
-                }
-                let w_: u8 = encode_neighbours(&im, i, j, w, true);
-                let n_w_: u8 = encode_neighbours(&im, i, j, w, false);
-                if (((TEMPL[ind1] & w_) == TEMPL[ind1])
-                    && ((N_TEMPL[ind1] & n_w_) == N_TEMPL[ind1]))
-                    || (((TEMPL[ind2] & w_) == TEMPL[ind2])
-                        && ((N_TEMPL[ind2] & n_w_) == N_TEMPL[ind2]))
-                    || (((TEMPL[ind3] & w_) == TEMPL[ind3])
-                        && ((N_TEMPL[ind3] & n_w_) == N_TEMPL[ind3]))
-                {
-                    diff = true;
-                    im[i * w + j] |= 2;
-                }
+    for i in min_y..max_y {
+        for j in min_x..max_x {
+            let p1: u8 = im[i * w + j] & 1;
+            if p1 == 0 {
+                continue;
+            }
+            let w_: u8 = encode_neighbours(&im, i, j, w, true);
+            let n_w_: u8 = encode_neighbours(&im, i, j, w, false);
+            if (((TEMPL[ind1] & w_) == TEMPL[ind1]) && ((N_TEMPL[ind1] & n_w_) == N_TEMPL[ind1]))
+                || (((TEMPL[ind2] & w_) == TEMPL[ind2])
+                    && ((N_TEMPL[ind2] & n_w_) == N_TEMPL[ind2]))
+                || (((TEMPL[ind3] & w_) == TEMPL[ind3])
+                    && ((N_TEMPL[ind3] & n_w_) == N_TEMPL[ind3]))
+            {
+                diff = true;
+                im[i * w + j] |= 2;
             }
         }
-        thinning_zs_post(im, win_x, win_y, win_w, win_h, w);
     }
     return diff;
 }
@@ -106,8 +103,11 @@ pub fn thinning_zs(im: &mut [u8], w: usize, h: usize) {
     loop {
         dbg!(iter);
         let mut diff = false;
-        if dbg!(thinning_zs_iteration(im, 0, 0, w, h, w, h)) {
-            diff = true;
+        for r in 1..5 {
+            if dbg!(thinning_zs_iteration(im, 0, 0, w, h, w, h, r)) {
+                thinning_zs_post(im, 0, 0, w, h, w);
+                diff = true;
+            }
         }
         if !diff {
             break;
@@ -158,38 +158,60 @@ pub fn thinning_zs_tiled(
         if remaining_tiles == 0 {
             break;
         }
-        let pb = ProgressBar::new(remaining_tiles as u64).with_message("Starting thinning");
+        let pb = ProgressBar::new(remaining_tiles as u64 * 4).with_message("Starting thinning");
         log::info!("Starting iteration {iter}, {remaining_tiles}/{total_tiles}");
         log::info!("Starting thinning");
 
+        for r in 1..5 {
+            for ti_y in 0..nty {
+                for ti_x in 0..ntx {
+                    if tile_flags[ti_y * ntx + ti_x] & FLAG_DONE != 0
+                        && (ti_x == 0 || tile_flags[ti_y * ntx + ti_x - 1] & FLAG_DONE != 0)
+                        && (ti_y == 0 || tile_flags[(ti_y - 1) * ntx + ti_x] & FLAG_DONE != 0)
+                        && (ti_x == ntx - 1 || tile_flags[ti_y * ntx + ti_x + 1] & FLAG_DONE != 0)
+                        && (ti_y == nty - 1 || tile_flags[(ti_y + 1) * ntx + ti_x] & FLAG_DONE != 0)
+                        && ((ti_y == nty - 1 || ti_x == ntx - 1)
+                            || tile_flags[(ti_y + 1) * ntx + ti_x + 1] & FLAG_DONE != 0)
+                        && ((ti_y == nty - 1 || ti_x == 0)
+                            || tile_flags[(ti_y + 1) * ntx + ti_x - 1] & FLAG_DONE != 0)
+                        && ((ti_y == 0 || ti_x == ntx - 1)
+                            || tile_flags[(ti_y - 1) * ntx + ti_x + 1] & FLAG_DONE != 0)
+                        && ((ti_y == 0 || ti_x == 0)
+                            || tile_flags[(ti_y - 1) * ntx + ti_x - 1] & FLAG_DONE != 0)
+                    {
+                        continue;
+                    }
+                    let win_x = ti_x * tile_width;
+                    let win_y = ti_y * tile_height;
+                    let win_w = tile_width.min(width - win_x);
+                    let win_h = tile_height.min(height - win_y);
+                    if thinning_zs_iteration(im, win_x, win_y, win_w, win_h, width, height, r) {
+                        tile_flags[ti_y * ntx + ti_x] |= FLAG_CHANGED;
+                    }
+                    pb.inc(1);
+                }
+            }
+            for ti_y in 0..nty {
+                for ti_x in 0..ntx {
+                    if tile_flags[ti_y * ntx + ti_x] & FLAG_CHANGED != 0 {
+                        let win_x = ti_x * tile_width;
+                        let win_y = ti_y * tile_height;
+                        let win_w = tile_width.min(width - win_x);
+                        let win_h = tile_height.min(height - win_y);
+                        thinning_zs_post(im, win_x, win_y, win_w, win_h, width);
+                    }
+                }
+            }
+        }
+
         for ti_y in 0..nty {
             for ti_x in 0..ntx {
-                if tile_flags[ti_y * ntx + ti_x] & FLAG_DONE != 0
-                    && (ti_x == 0 || tile_flags[ti_y * ntx + ti_x - 1] & FLAG_DONE != 0)
-                    && (ti_y == 0 || tile_flags[(ti_y - 1) * ntx + ti_x] & FLAG_DONE != 0)
-                    && (ti_x == ntx - 1 || tile_flags[ti_y * ntx + ti_x + 1] & FLAG_DONE != 0)
-                    && (ti_y == nty - 1 || tile_flags[(ti_y + 1) * ntx + ti_x] & FLAG_DONE != 0)
-                    && ((ti_y == nty - 1 || ti_x == ntx - 1)
-                        || tile_flags[(ti_y + 1) * ntx + ti_x + 1] & FLAG_DONE != 0)
-                    && ((ti_y == nty - 1 || ti_x == 0)
-                        || tile_flags[(ti_y + 1) * ntx + ti_x - 1] & FLAG_DONE != 0)
-                    && ((ti_y == 0 || ti_x == ntx - 1)
-                        || tile_flags[(ti_y - 1) * ntx + ti_x + 1] & FLAG_DONE != 0)
-                    && ((ti_y == 0 || ti_x == 0)
-                        || tile_flags[(ti_y - 1) * ntx + ti_x - 1] & FLAG_DONE != 0)
-                {
-                    continue;
-                }
-                let win_x = ti_x * tile_width;
-                let win_y = ti_y * tile_height;
-                let win_w = tile_width.min(width - win_x);
-                let win_h = tile_height.min(height - win_y);
-                if thinning_zs_iteration(im, win_x, win_y, win_w, win_h, width, height) {
-                    tile_flags[ti_y * ntx + ti_x] |= FLAG_CHANGED;
-                } else {
+                if tile_flags[ti_y * ntx + ti_x] & FLAG_CHANGED == 0 {
                     tile_flags[ti_y * ntx + ti_x] = FLAG_DONE;
+                } else {
+                    tile_flags[ti_y * ntx + ti_x] &= !FLAG_DONE;
+                    tile_flags[ti_y * ntx + ti_x] &= !FLAG_CHANGED;
                 }
-                pb.inc(1);
             }
         }
         pb.finish();
@@ -250,6 +272,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     // let height = 599280;
 
     // thinning_zs(im, width, height);
+    let (tile_width, tile_height) = (8, 8);
     thinning_zs_tiled(im, width, height, tile_width, tile_height);
 
     // let skeleton = skeleton::trace_skeleton(im, width, height, 0, 0, 100000, 100000, 10, 999);
